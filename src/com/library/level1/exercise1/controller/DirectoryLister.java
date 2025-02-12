0edf2c5540c00196d0f93c7a38b335a120c35d2b
acocinas <alcogar79@gmail.com>
2025-02-12 10:49:24 +0100

    improving exercise1 with a loop instead of an 'if'

--- src/com/library/level1/exercise1/controller/DirectoryLister.java
+++ src/com/library/level1/exercise1/controller/DirectoryLister.java
@@ -1,22 +1,29 @@
 package com.library.level1.exercise1.controller;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class DirectoryLister {
-    public static void run (String path){
+    public static void run(String path) {
         Scanner sc = new Scanner(System.in);
-        if (path == null || path.isBlank()){
+        while (path == null || path.isBlank() || !isValidFile(path)) {
             System.out.println("Introduce el directorio path: ");
             path = sc.nextLine().trim();
+            path = path.replace("/", File.separator).replace("\\", File.separator);
         }
+//        if (path == null || path.isBlank()){
+//            System.out.println("Introduce el directorio path: ");
+//            path = sc.nextLine().trim();
+//        }
         sc.close();
 
-        path = path.replace("/", File.separator).replace("\\", File.separator);
 
         File directory = new File(path);
-        if (!directory.exists() || !directory.isDirectory()){
+        if (!directory.exists() || !directory.isDirectory()) {
             System.out.println("Invalid directory path");
             System.exit(1);
         }
@@ -24,9 +31,13 @@ public class DirectoryLister {
         File[] files = directory.listFiles();
         if (files != null) {
             Arrays.sort(files);
-            for (File file : files){
+            for (File file : files) {
                 System.out.println(file.getName());
             }
         }
+    }
+    private static boolean isValidFile(String path){
+        Path filePath = Paths.get(path);
+        return Files.exists(filePath);
     }
 }
