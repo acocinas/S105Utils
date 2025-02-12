9a054d1f400241bceb817e7d4dee3984a63c1674
acocinas <alcogar79@gmail.com>
2025-02-12 10:34:39 +0100

    fixing a bug in the import package

--- src/com/library/level1/exercise1/Main.java
+++ src/com/library/level1/exercise1/Main.java
@@ -1,10 +1,10 @@
 package com.library.level1.exercise1;
 
-import com.library.level1.exercise2.controller.DirectoryTreeLister;
+import com.library.level1.exercise1.controller.DirectoryLister;
 
 public class Main {
     public static void main(String[] args){
         String path = (args.length > 0) ? args[0] : null;
-        DirectoryTreeLister.run(path);
+        DirectoryLister.run(path);
     }
 }
